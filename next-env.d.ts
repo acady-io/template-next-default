9cf86e3b790ce2eb489d153c1fecb3a75e9d6a29
schab <christian.schab@wusoma.com>
2021-01-13 13:39:38 +0100

    Optimizing template for deployments

--- next-env.d.ts
+++ next-env.d.ts
@@ -1,2 +1,18 @@
 /// <reference types="next" />
 /// <reference types="next/types/global" />
+
+/**
+ * Default CSS definition for typescript,
+ * will be overridden with file-specific definitions by rollup
+ */
+declare module '*.css' {
+    const content: { [className: string]: string };
+    export default content;
+}
+
+declare module '*.scss';
+
+declare module '*.svg' {
+    const content: any;
+    export default content;
+}
